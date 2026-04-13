93bbf772d011f84e4f986c6ae2c73a3de300471a
tyabus <tyabustest@gmail.com>
2026-04-13 15:09:36 +0500

    [ci skip] vphysics_jolt: Drop GAME_DESOLATION support

--- vphysics_jolt/vjolt_interface.h
+++ vphysics_jolt/vjolt_interface.h
@@ -8,10 +8,9 @@
 
 //-------------------------------------------------------------------------------------------------
 
-// Whether to use the VPhysics debug overlay (legacy compatible) or the fully featured IVDebugOverlay
-// In Desolation, IVDebugOverlay has new members that allow us to draw IMesh objects, without this
+// Whether to use the VPhysics debug overlay (legacy compatible)
 // debugoverlay rendering is incredibly inefficient (and may run the materialsystem mempool out of memory)
-#if !defined( GAME_VITAMIN ) || defined( BUILD_FOR_EXTERNAL_GAME )
+#if !defined( BUILD_FOR_EXTERNAL_GAME )
 #define VJOLT_USE_PHYSICS_DEBUG_OVERLAY
 #endif
 
